292f4b68a4ace7adeb7f5d9eef36fb11181af3ca
Devesh Sharma <dev1710.sharma@gmail.com>
2020-08-30 04:26:17 +0530

    all ticket routes are completed

--- Routes/ticket.js
+++ Routes/ticket.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const Ticket = require("../models/ticket.js");
 const mongoose = require("mongoose");
 
-router.post("/newticket", (req, res) => {
+router.post("/ticket/new", (req, res) => {
   let id = req.body.userid;
   let phonenum = req.body.phone;
-  let date = req.body.date;
+  let date1 = req.body.timing;
   let newticket = new Ticket({
     userid: id,
     phone: phonenum,
-    date: date,
+    timing: date1,
   });
   Ticket.create(newticket)
     .then(() => {
@@ -22,5 +22,64 @@ router.post("/newticket", (req, res) => {
       res.status(500).send(err);
     });
 });
+
+//update ticket router
+
+router.put("/update/:id", (req, res) => {
+  //   console.log(req.params);
+  Ticket.findByIdAndUpdate(req.params.id, { $set: { timing: req.body.date } })
+    .then((updatedticket) => {
+      console.log("successfully update");
+      res.status(200).send(updatedticket);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+//delete a ticket router
+
+router.delete("/delete/:id", (req, res) => {
+  Ticket.findByIdAndDelete(req.params.id)
+    .then(() => {
+      console.log("successfully deleted");
+      res.status(200).send();
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+//Get all the tickets
+
+router.get("/view", (req, res) => {
+  console.log("inside vview router");
+  Ticket.find({ timing: req.body.timing })
+    .then((tickets) => {
+      res.status(200).send(tickets);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+//get  user from itcket id
+
+router.get("/user/:id", (req, res) => {
+  try {
+    Ticket.findById(req.params.id)
+      .populate("userid")
+      .exec(function (err, ticket) {
+        try {
+          res.status(200).json(ticket.userid);
+        } catch (error) {
+          res.status(500).send(err);
+        }
+      });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+//====================
 
 module.exports = router;
